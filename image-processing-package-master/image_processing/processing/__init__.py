95699266da36fd9d591eaec7919ea8e1ebdb8503
Diogo Oliveira <168039819+DiogoOliveiraR@users.noreply.github.com>
2025-04-25 15:10:21 -0300

    Add files via upload

---
+++ image-processing-package-master/image_processing/processing/__init__.py
@@ -0,0 +1,4 @@
+from .io import carregar_imagem
+from .plot import mostrar_imagem
+
+__all__ = ['carregar_imagem', 'mostrar_imagem']
---
+++ image-processing-package-master/image_processing/utils/io.py
@@ -0,0 +1,3 @@
+def saudacao(nome):
+    """Função simples de saudação."""
+    return f"Olá, {nome}! Bem-vindo ao pacote de processamento de imagens."
